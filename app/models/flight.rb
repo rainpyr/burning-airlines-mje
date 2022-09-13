8e91a81669f8f20ed787fea10865941ce7e5d737
Mohamad Dakdouk <mdak2806@gmail.com>
2022-09-14 09:54:03 +1000

    fix res

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,6 @@ class Flight < ApplicationRecord
 
     belongs_to :plane, optional: true
     has_many :reservations
-    has_many :users, through: 'reservatiions'
+    has_many :users, through: 'reservations'
 
 end
