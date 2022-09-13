c910f8299401e50dc017f57446b6cff7acdcfb46
Justin Tan <justintws10@gmail.com>
2022-09-13 20:10:48 +1000

    created Plane model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+print "Creating planes..."
+
+Plane.destroy_all
+
+p1 = Plane.create!(
+    name: "Eva",
+    rows: 3,
+    cols: 4
+)
+
+p2 = Plane.create!(
+    name: "Justin",
+    rows: 2,
+    cols: 5
+)
+
+p3 = Plane.create!(
+    name: "Moe",
+    rows: 3,
+    cols: 5
+)
+
+puts "created #{Plane.count} planes"
